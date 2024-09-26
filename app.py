f95c7e862c5d041dba67b4610544114fab8e2f72
Halima Ehsan <halimabutt33@gmail.com>
2024-09-26 18:19:04 +0500

    database updated

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
-    print(app.config)
+    
     db.init_app(app)
 
     from routes import bp as main_bp
