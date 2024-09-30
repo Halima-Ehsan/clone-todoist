3e1a2f5d03279c3806528325c114f92a78a2ed68
Halima Ehsan <halimabutt33@gmail.com>
2024-09-30 17:33:37 +0500

    Database updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    #app.config.from_object('config.Config')
+    app.config.from_object('config.Config')
 
     app.config['SQLALCHEMY_DATABASE_URI'] = (
         'mysql+pymysql://avnadmin:AVNS_M4XPUGHtiJz3RctIKmo@mirzaahmergull-aiven-mysql-test-project-bunnybeans.k.aivencloud.com:10555/defaultdb'
